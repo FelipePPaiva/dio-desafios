039c851a1bdbefc36e9105c467f24caf9f52e726
Felipe_Paiva <fpaiva.design@gmail.com>
2022-12-09 18:06:11 -0300

    Aula sobre variaveis e operadores e primeiro desafio

---
+++ javascript/aula.js
@@ -0,0 +1,17 @@
+// Let --> Valor pode ser alterado
+// Const --> Valor não pode ser alterado
+
+// Operadores aritiméticos
+let numero = 10;
+let numero2 = 20;
+// Soma
+console.log(numero + numero2);
+
+// Subtração
+console.log(numero - numero2);
+
+// Multiplicação
+console.log(numero * numero2);
+
+// Divisão
+console.log(numero / numero2);
---
+++ javascript/primeiro_desafio.js
@@ -0,0 +1,7 @@
+const combustivel = 5.74;
+let kmPorLitro = 10.5;
+let distancia_viagem_km = 535;
+
+let totalGasto = (distancia_viagem_km/kmPorLitro)*combustivel;
+
+console.log("O valor gasto na sua viagem será de R$"+ totalGasto);

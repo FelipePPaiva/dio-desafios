bae3e01760b879c226eb096efe0335ef7d106e71
Felipe_Paiva <fpaiva.design@gmail.com>
2022-12-16 21:08:44 -0300

    Aulas e desafio sobre Arrays e estrutura de repetição

---
+++ javascript/08 - arrays e estruturas de repetição/aula.js
@@ -0,0 +1,16 @@
+const nomes = ['Felipe','Rosi','Jardel'];
+
+nomes.push('Vitor');
+
+nomes[2] = 'Cleiton'
+
+nomes.pop();
+nomes.shift();
+
+console.log(nomes);
+
+const nome = 'Felipe Pereira Paiva';
+
+for(i = 0; i < nome.length; i++){
+    console.log(nome[i]);
+}
